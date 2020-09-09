295bf2b37cbdf0975a985b6f552dccdfe5e5c106
Manole Mădălina <67560247+ManoleMadalina@users.noreply.github.com>
2020-09-09 16:16:24 +0300

    Update contact-form.php

--- contact-form.php
+++ contact-form.php
@@ -1,22 +1,39 @@
 <?php
-    $name=$_POST['name'];
-    $visitor_phone= $_POST['phone'];
-    $message=$_POST['message'];
+require 'PHPMailerAutoload.php';
 
-    $email_from = 'madalinamanole71@gmail.com';
-    $email_subject = "New Form Submission";
-    $email_body = "User Name: $name.\n".
-                  "User Phone: $visitor_phone.\n".
-                  "User Message: $message.\n";
+$mail = new PHPMailer;
 
-    $to = "WebFormulaTeam@gmail.com";
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+$name=$_POST['name'];
+$phone=$_POST['phone'];
+$message=$_POST['message'];
 
-    $headers = "From: $email_from \r\n";
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'madalinamanole71@gmail.com';                 // SMTP username
+$mail->Password = 'madalina1999';                           // SMTP password
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
 
-    $headers .= "Reply-To: $visitor_phone \r\n";
+$mail->setFrom('madalinamanole71@gmail.com');
+$mail->addAddress('WebFormulaTeam@gmail.com');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
 
-    mail($to, $email_subject, $email_body, $headers);
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
 
-    header("Location: index0.html");
-                  
-?>
+$mail->Subject = 'Test Email';
+$mail->Body    = ''.$name . 'a lasat numarul de telefon: ' .$phone. '<br> Mesaj: ' .$message;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
